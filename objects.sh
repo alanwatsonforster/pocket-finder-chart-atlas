7a3c9131921277ee7ae34428a1924ce2140d6431
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2022-02-24 01:45:42 -0600

    Start creating our own deep-sky catalog.

---
+++ objects.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+awk -F, '
+NR == 1 {
+  next;
+}
+NR > 2 {
+  if (id1 != $1 || id2 != $2)
+    printf("%s%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", \
+      id1, id2, alpha, delta, x, y, pa, type, \
+      cat_M, cat_C, cat_U, cat_NGC, cat_IC, cat_Mel, cat_Cr, name);
+}
+{
+  id1 = $1;
+  id2 = $2; 
+}
+$5 != "" {
+  alpha = $6;
+}
+$6 != "" {
+  delta = $6;
+}
+$7 != "" {
+  x = $7;
+}
+$8 != "" {
+  y = $8;
+}
+$9 != "" {
+  pa = $9;
+}
+$10 != "" {
+  type = $10;
+}
+$11 != "" {
+  cat_M = $11;
+}
+$12 != "" {
+  cat_C = $12;
+}
+$13 != "" {
+  cat_U = $13;
+}
+$14 != "" {
+  cat_NGC = $14;
+}
+$15 != "" {
+  cat_IC = $15;
+}
+$16 != "" {
+  cat_Mel = $16;
+}
+$17 != "" {
+  cat_Cr = $17;
+}
+$18 != "" {
+  name = $18;
+}
+' objects-raw.csv >objects.csv
