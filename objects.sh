aa7a4f5d24f39ce4aa841bfd1dcf9a2ad699f754
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2022-03-06 20:44:36 -0600

    Fixed bugs in processing.

--- objects.sh
+++ objects.sh
@@ -1,15 +1,46 @@
 #!/bin/sh
 
-sort -t, -k "1,1" -k "2,2n" -k "3,3n" objects-raw.csv |
+sed "1d" objects-raw.csv |
+sort -t, -k "1,1" -k "2,2n" -k "3,3n"  |
 awk -F, '
-NR == 1 {
-  next;
+function printobject() {
+  printf("%s%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", \
+    id1, id2, alpha, delta, x, y, pa, type, \
+    cat_M, cat_C, cat_U, cat_NGC, cat_IC, cat_Mel, cat_Cr, name);
+  if (alpha == "~") 
+    print("# alpha missing");
+  if (delta == "~")
+    print("# delta missing");
+  if (type == "")
+    print("# type missing");
+  if (x == "~" && type != "FC")
+    print("# x missing");
+  if (y == "~" && type == "GAL")
+    print("# y missing");
+  if (pa == "~" && type == "GAL")
+    print("# pa missing");
+  alpha = "~";
+  delta = "~";
+  x = "~";
+  y = "~";
+  pa = "~";
+  type = "";
+  cat_M = "";
+  cat_C = "";
+  cat_U = "";
+  cat_NGC = "";
+  cat_IC = "";
+  cat_Mel = "";
+  cat_Cr = "";
+  name = "";
 }
-NR > 2 {
-  if (id1 != $1 || id2 != $2)
-    printf("%s%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", \
-      id1, id2, alpha, delta, x, y, pa, type, \
-      cat_M, cat_C, cat_U, cat_NGC, cat_IC, cat_Mel, cat_Cr, name);
+NR > 1 {
+  if (id1 != $1 || id2 != $2) {
+    printobject()
+  }    
+}
+END {
+  printobject();
 }
 {
   id1 = $1;
