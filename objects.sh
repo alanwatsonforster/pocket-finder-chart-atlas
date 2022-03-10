d39c3fd80251cb054bd04c894a83eb51d0859a54
alanwatsonforster <68709385+alanwatsonforster@users.noreply.github.com>
2022-03-10 08:20:29 -0600

    Added PSA charts.

--- objects.sh
+++ objects.sh
@@ -4,8 +4,8 @@ sed "1d" objects-raw.csv |
 sort -t, -k "1,1" -k "2,2n" -k "3,3n"  |
 awk -F, '
 function printobject() {
-  printf("%s%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", \
-    id1, id2, alpha, delta, x, y, pa, type, \
+  printf("%s%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n", \
+    id1, id2, alpha, delta, psa, x, y, pa, type, \
     cat_M, cat_C, cat_U, cat_NGC, cat_IC, cat_Mel, cat_Cr, name);
   if (alpha == "~") 
     print("# alpha missing");
@@ -53,39 +53,42 @@ $6 != "" {
   delta = $6;
 }
 $7 != "" {
-  x = $7;
+  psa = $7;
 }
 $8 != "" {
-  y = $8;
+  x = $8;
 }
 $9 != "" {
-  pa = $9;
+  y = $9;
 }
 $10 != "" {
-  type = $10;
+  pa = $10;
 }
 $11 != "" {
-  cat_M = $11;
+  type = $11;
 }
 $12 != "" {
-  cat_C = $12;
+  cat_M = $12;
 }
 $13 != "" {
-  cat_U = $13;
+  cat_C = $13;
 }
 $14 != "" {
-  cat_NGC = $14;
+  cat_U = $14;
 }
 $15 != "" {
-  cat_IC = $15;
+  cat_NGC = $15;
 }
 $16 != "" {
-  cat_Mel = $16;
+  cat_IC = $16;
 }
 $17 != "" {
-  cat_Cr = $17;
+  cat_Mel = $17;
 }
 $18 != "" {
-  name = $18;
+  cat_Cr = $18;
+}
+$19 != "" {
+  name = $19;
 }
 ' >objects.csv
