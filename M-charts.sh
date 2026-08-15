25a2a307038d3d2a9d8ef7ef6955b53a8df31b28
Alan Watson Forster <68709385+alanwatsonforster@users.noreply.github.com>
2026-08-15 13:58:24 -0600

    Move to new format with integrated data

---
+++ M-charts.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+sort -t, -k "9,9n" objects.csv |
+awk -F, '
+NR > 1 && $9 != "" {
+  alpha = $2;
+  delta = $3;
+  psa = $4;
+  x = $5;
+  y = $6;
+  pa = $7;
+  type = $8;
+  cat_M   = $9;
+  cat_C   = $10;
+  cat_U   = $11;
+  cat_NGC = $12;
+  cat_IC  = $13;
+  cat_Mel = $14;
+  cat_Cr  = $15;
+  name    = $16;
+  if (type == "GAL")
+    type = "Gal";
+
+  printf("\\chart{M%d}", cat_M);
+
+  printf("{")
+  if (cat_U != "") {
+    printf("\\\\U%s", cat_U)
+  }
+  if (cat_NGC != "") {
+    printf("\\\\NGC %s", cat_NGC)
+  } else if (cat_IC != "") {
+    printf("\\\\IC %s", cat_IC)
+  } else if (cat_Mel != "") {
+    printf("\\\\Mel %s", cat_Mel)
+  } else if (cat_Cr != "") {
+    printf("\\\\Cr %s", cat_Cr)
+  }
+  if (name != "") {
+    printf("\\\\%s", name)
+  }
+  printf("}")
+  printf("{\\chartdata{%04.1f}{%+03.0f}{}{%s}{}{}{%s}{}{}}", alpha / 15, delta, psa, type)
+  printf("{}{}{}\n")
+}
+' >M-charts.tex
---
+++ U-charts.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+sort -t, -k "11,11n" objects.csv |
+awk -F, '
+NR > 1 && $11 != "" {
+  alpha = $2;
+  delta = $3;
+  psa = $4;
+  x = $5;
+  y = $6;
+  pa = $7;
+  type = $8;
+  cat_M   = $9;
+  cat_C   = $10;
+  cat_U   = $11;
+  cat_NGC = $12;
+  cat_IC  = $13;
+  cat_Mel = $14;
+  cat_Cr  = $15;
+  name    = $16;
+  if (type == "GAL")
+    type = "Gal";
+
+  printf("\\chart{U%d}", cat_U);
+
+  printf("{")
+  if (cat_M != "") {
+    printf("\\\\M%s", cat_M)
+  }
+  if (cat_C != "") {
+    printf("\\\\C%s", cat_C)
+  }
+  if (cat_NGC != "") {
+    printf("\\\\NGC %s", cat_NGC)
+  } else if (cat_IC != "") {
+    printf("\\\\IC %s", cat_IC)
+  } else if (cat_Mel != "") {
+    printf("\\\\Mel %s", cat_Mel)
+  } else if (cat_Cr != "") {
+    printf("\\\\Cr %s", cat_Cr)
+  }
+  if (name != "") {
+    printf("\\\\%s", name)
+  }
+  printf("}")
+  printf("{\\chartdata{%04.1f}{%+03.0f}{}{%s}{}{}{%s}{}{}}", alpha / 15, delta, psa, type)
+  printf("{}{}{}\n")
+}
+' >U-charts.tex
